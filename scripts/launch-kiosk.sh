756b0cc13173624d9278b043311607fc10656a8f
Matt Cobb <matt@reedsy.com>
2025-04-19 15:48:49 +0100

    Attempt force dark-mode

--- scripts/launch-kiosk.sh
+++ scripts/launch-kiosk.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-chromium-browser http://localhost:3000 --kiosk --no-sandbox --disable-web-security --disable-webusb-security --disable-extensions --autoplay-policy=no-user-gesture-required --window-size=1600,600 --start-fullscreen --default-background-color=000000 --dark_vibrant &
+chromium-browser http://localhost:3000 \
+  --kiosk \
+  --no-sandbox \
+  --disable-web-security \
+  --disable-webusb-security \
+  --disable-extensions \
+  --autoplay-policy=no-user-gesture-required \
+  --window-size=1600,600 \
+  --start-fullscreen \
+  --default-background-color=000000 \
+  --force-dark-mode &
