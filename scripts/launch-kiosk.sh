4977c7ffba16de25e165240e9b857654127a0e6b
Matt Cobb <matt@reedsy.com>
2025-04-19 15:14:19 +0100

    Attempt transparent background color

--- scripts/launch-kiosk.sh
+++ scripts/launch-kiosk.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chromium-browser http://localhost:3000 --kiosk --no-sandbox --disable-web-security --disable-webusb-security --disable-extensions --autoplay-policy=no-user-gesture-required --window-size=1600,600 --start-fullscreen &
+chromium-browser http://localhost:3000 --kiosk --no-sandbox --disable-web-security --disable-webusb-security --disable-extensions --autoplay-policy=no-user-gesture-required --window-size=1600,600 --start-fullscreen --default-background-color=00000000 &
