f34243ea0a35e0a35befcd74f4c4e45edde073be
Matt Cobb <matt@reedsy.com>
2025-04-20 21:41:08 +0100

    Add carpihat-shutdown.service

--- scripts/pi-shutdown-init.py
+++
@@ -1,9 +0,0 @@
-# ------------------------------------------
-# init.py
-from gpiozero import Button
-import time
-
-button = Button(12, pull_up=False)
-button.close()
-
-print("Initiated carPiHat GPIO 12")
